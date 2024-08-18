684ac2dc6843e60c93cdd4990678bb9e48be3f3d
NinaIvljanin <ninaivljanin001@gmail.com>
2024-08-18 21:25:36 +0200

    Osnove kalendara

--- react_aplikacija/src/App.js
+++ react_aplikacija/src/App.js
@@ -1,5 +1,5 @@
 
-
+/*
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBarLogin'; 
@@ -37,5 +37,40 @@ function App() {
   );
 }
 
+export default App;*/
+
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBarLogin'; // Pretpostavljam da je to nav bar za početnu stranicu
+import LoginPage from './components/LoginPage';
+import DashboardPage from './components/DashboardPage';
+import HomePage from './components/HomePage';
+import MyCalendar from './components/MyCalendar';
+import AllEvents from './components/AllEvents';
+import Logout from './components/Logout';
+import CalendarPage from './components/Calendar'; // Importuj CalendarPage
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <NavBar /> {/* Pretpostavljam da je to nav bar sa Login i Calendar */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/my-calendar" element={<MyCalendar />} />
+          <Route path="/all-events" element={<AllEvents />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/calendar" element={<CalendarPage />} /> {/* Dodaj ovu rutu */}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
 export default App;
+
 
--- react_aplikacija/src/components/NavBarLogin.jsx
+++ react_aplikacija/src/components/NavBarLogin.jsx
@@ -1,4 +1,4 @@
-
+/*
 import React from 'react';
 import { IoPersonCircleOutline } from "react-icons/io5";
 import { RxCalendar } from "react-icons/rx";
@@ -29,4 +29,33 @@ const NavBar = () => {
 };
 
 export default NavBar;
+*/
 
+import React from 'react';
+import { IoPersonCircleOutline } from "react-icons/io5";
+import { RxCalendar } from "react-icons/rx";
+import { Link } from 'react-router-dom';
+
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-links">
+          <div className="navBar">
+            <Link to="/login" className="navBarItem">
+              <IoPersonCircleOutline /> <b>Login</b>
+            </Link>
+
+            <Link to="/calendar" className="navBarItem">
+              <RxCalendar /> <b>Calendar</b>
+            </Link>
+
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
