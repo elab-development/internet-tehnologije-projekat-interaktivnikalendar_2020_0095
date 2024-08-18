1673b9cf9bd3b2f912607c2939b3caff625728ea
NinaIvljanin <ninaivljanin001@gmail.com>
2024-08-18 16:29:33 +0200

    Sredjena pocetna strana

--- react_aplikacija/src/App.js
+++ react_aplikacija/src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
 import NavBar from './components/NavBar';
+import slika from './images/slika.jpg';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
-      {/*prom*/}
+      <div className = "hero-container"><img src = {slika} alt = "Business Calendar Background" className = "hero-image"/>
+          <div className = "hero-text">   Welcome to Your Business Calendar</div>
+      </div>
     </div>
   );
 }
