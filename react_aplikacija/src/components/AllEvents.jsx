c470174aea2169d4803047b695bde82bfbcbe296
aleksandradjordjevic0307 <152723800+aleksandradjordjevic0307@users.noreply.github.com>
2024-08-19 21:18:18 +0200

    Dodata funkcionalnost AllEvents

--- react_aplikacija/src/components/AllEvents.jsx
+++ react_aplikacija/src/components/AllEvents.jsx
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const AllEvents = () => {
   const location = useLocation();
-  const { events } = location.state || { events: [] }; 
+  const [events, setEvents] = useState(location.state?.events || []); 
+
+  const handleDeleteEvent = (indexToDelete) => {
+    const updatedEvents = events.filter((_, index) => index !== indexToDelete);
+    setEvents(updatedEvents);
+  };
 
   return (
-    <div>
-      <h2>All Events</h2>
+    <div className="all-events">
+      <h2 className="allEventsTitle">All Events</h2>
       {events.length === 0 ? (
         <p>No events to display.</p>
       ) : (
-        <ul>
+        <ul className="events-list">
           {events.map((event, index) => (
-            <li key={index}>
-              {event.date.toDateString()}: {event.title}
+            <li key={index} className="event-item">
+              <div className="event-details">
+                <span className="event-date">{event.date.toDateString()}</span>
+                <span className="event-title">{event.title}</span>
+                <span className="event-description">{event.description}</span>
+                <span className="event-time">{event.time}</span>
+              </div>
+              <input
+                type="checkbox"
+                onChange={() => handleDeleteEvent(index)}
+                className="event-checkbox"
+              />
             </li>
           ))}
         </ul>
@@ -24,3 +39,4 @@ const AllEvents = () => {
 };
 
 export default AllEvents;
+
